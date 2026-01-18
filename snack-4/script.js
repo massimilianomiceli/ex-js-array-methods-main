c9e1e78447ee6d84ded3b72f4f660c243e5e4b98
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-01-18 15:39:15 +0100

    snack 4 completato

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,7 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const postsAuthorsNames = posts.map ((singlePost) => singlePost.author);
+console.log(postsAuthorsNames);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
