ff1a98a47c7da094265d8656978eb811cf596cd6
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-01-18 15:41:21 +0100

    snack 5 completato

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,6 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const numsPari = nums.filter((number => number % 2 === 0));
+console.log(numsPari);
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
