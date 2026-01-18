aef292cf5d8e06a9ebc81ffd7a66978e6b7f9a5c
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-01-18 15:31:49 +0100

    snack 1 completato

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+names.forEach((singleName) => {
+   console.log(singleName); 
+});
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
